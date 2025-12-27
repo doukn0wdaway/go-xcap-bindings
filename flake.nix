eaddc87030543d9481d3e1b99a0e89bd7d856e7d
doukn0wdaway <anisimov.sasha20@gmail.com>
2025-12-27 22:43:59 +0400

    xcap version

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "golang xcap bindings dev flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+
+        devShells.default = pkgs.mkShell {
+
+
+
+          buildInputs = [
+            pkgs.rustc
+            pkgs.pkg-config
+            pkgs.cargo
+            pkgs.wayland
+
+            pkgs.mesa
+            pkgs.libgbm
+            pkgs.pipewire
+            pkgs.libGL
+            pkgs.clang
+
+            pkgs.libxcb
+            # pkgs.pkgsCross.musl64.cargo
+          ];
+
+
+          shellHook = ''
+            export CC=clang
+            export LIBCLANG_PATH=${pkgs.llvmPackages_20.libclang.lib}/lib
+            export CXX=clang++
+          '';
+        };
+      });
+}
